6fd6111cd214d2e9ddca427d078cbed3cc10ac82
Mark Wetzel <mark@markwetzel.com>
2013-06-12 20:32:47 -0700

    Added a few server configuration options.

--- Server/ConfigurationForm.Designer.cs
+++ Server/ConfigurationForm.Designer.cs
@@ -29,16 +29,22 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabServer = new System.Windows.Forms.TabPage();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblIpAddress = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
             this.tabUsers = new System.Windows.Forms.TabPage();
             this.tabRooms = new System.Windows.Forms.TabPage();
-            this.txtIpAddress = new System.Windows.Forms.TextBox();
-            this.txtPort = new System.Windows.Forms.TextBox();
-            this.lblIpAddress = new System.Windows.Forms.Label();
-            this.lblPort = new System.Windows.Forms.Label();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.txtMaxUsers = new System.Windows.Forms.TextBox();
+            this.lblMaxUsers = new System.Windows.Forms.Label();
+            this.lblMaxRooms = new System.Windows.Forms.Label();
+            this.txtMaxRooms = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabServer.SuspendLayout();
+            this.tabUsers.SuspendLayout();
+            this.tabRooms.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -66,37 +72,14 @@
             this.tabServer.Text = "Server";
             this.tabServer.UseVisualStyleBackColor = true;
             // 
-            // tabUsers
+            // lblPort
             // 
-            this.tabUsers.Location = new System.Drawing.Point(4, 22);
-            this.tabUsers.Name = "tabUsers";
-            this.tabUsers.Size = new System.Drawing.Size(539, 391);
-            this.tabUsers.TabIndex = 1;
-            this.tabUsers.Text = "Users";
-            this.tabUsers.UseVisualStyleBackColor = true;
-            // 
-            // tabRooms
-            // 
-            this.tabRooms.Location = new System.Drawing.Point(4, 22);
-            this.tabRooms.Name = "tabRooms";
-            this.tabRooms.Size = new System.Drawing.Size(539, 391);
-            this.tabRooms.TabIndex = 2;
-            this.tabRooms.Text = "Rooms";
-            this.tabRooms.UseVisualStyleBackColor = true;
-            // 
-            // txtIpAddress
-            // 
-            this.txtIpAddress.Location = new System.Drawing.Point(94, 18);
-            this.txtIpAddress.Name = "txtIpAddress";
-            this.txtIpAddress.Size = new System.Drawing.Size(100, 20);
-            this.txtIpAddress.TabIndex = 0;
-            // 
-            // txtPort
-            // 
-            this.txtPort.Location = new System.Drawing.Point(94, 44);
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(100, 20);
-            this.txtPort.TabIndex = 1;
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(21, 47);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 4;
+            this.lblPort.Text = "Port";
             // 
             // lblIpAddress
             // 
@@ -107,14 +90,41 @@
             this.lblIpAddress.TabIndex = 3;
             this.lblIpAddress.Text = "IP Address";
             // 
-            // lblPort
+            // txtPort
             // 
-            this.lblPort.AutoSize = true;
-            this.lblPort.Location = new System.Drawing.Point(21, 47);
-            this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(26, 13);
-            this.lblPort.TabIndex = 4;
-            this.lblPort.Text = "Port";
+            this.txtPort.Location = new System.Drawing.Point(94, 44);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(94, 18);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtIpAddress.TabIndex = 0;
+            // 
+            // tabUsers
+            // 
+            this.tabUsers.Controls.Add(this.lblMaxUsers);
+            this.tabUsers.Controls.Add(this.txtMaxUsers);
+            this.tabUsers.Location = new System.Drawing.Point(4, 22);
+            this.tabUsers.Name = "tabUsers";
+            this.tabUsers.Size = new System.Drawing.Size(539, 350);
+            this.tabUsers.TabIndex = 1;
+            this.tabUsers.Text = "Users";
+            this.tabUsers.UseVisualStyleBackColor = true;
+            // 
+            // tabRooms
+            // 
+            this.tabRooms.Controls.Add(this.lblMaxRooms);
+            this.tabRooms.Controls.Add(this.txtMaxRooms);
+            this.tabRooms.Location = new System.Drawing.Point(4, 22);
+            this.tabRooms.Name = "tabRooms";
+            this.tabRooms.Size = new System.Drawing.Size(539, 350);
+            this.tabRooms.TabIndex = 2;
+            this.tabRooms.Text = "Rooms";
+            this.tabRooms.UseVisualStyleBackColor = true;
             // 
             // btnOk
             // 
@@ -136,6 +146,38 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // txtMaxUsers
+            // 
+            this.txtMaxUsers.Location = new System.Drawing.Point(97, 16);
+            this.txtMaxUsers.Name = "txtMaxUsers";
+            this.txtMaxUsers.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxUsers.TabIndex = 5;
+            // 
+            // lblMaxUsers
+            // 
+            this.lblMaxUsers.AutoSize = true;
+            this.lblMaxUsers.Location = new System.Drawing.Point(25, 23);
+            this.lblMaxUsers.Name = "lblMaxUsers";
+            this.lblMaxUsers.Size = new System.Drawing.Size(57, 13);
+            this.lblMaxUsers.TabIndex = 6;
+            this.lblMaxUsers.Text = "Max Users";
+            // 
+            // lblMaxRooms
+            // 
+            this.lblMaxRooms.AutoSize = true;
+            this.lblMaxRooms.Location = new System.Drawing.Point(21, 27);
+            this.lblMaxRooms.Name = "lblMaxRooms";
+            this.lblMaxRooms.Size = new System.Drawing.Size(63, 13);
+            this.lblMaxRooms.TabIndex = 8;
+            this.lblMaxRooms.Text = "Max Rooms";
+            // 
+            // txtMaxRooms
+            // 
+            this.txtMaxRooms.Location = new System.Drawing.Point(93, 20);
+            this.txtMaxRooms.Name = "txtMaxRooms";
+            this.txtMaxRooms.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxRooms.TabIndex = 7;
+            // 
             // ConfigurationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -150,6 +192,10 @@
             this.tabControl1.ResumeLayout(false);
             this.tabServer.ResumeLayout(false);
             this.tabServer.PerformLayout();
+            this.tabUsers.ResumeLayout(false);
+            this.tabUsers.PerformLayout();
+            this.tabRooms.ResumeLayout(false);
+            this.tabRooms.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -166,5 +212,9 @@
         private System.Windows.Forms.TextBox txtIpAddress;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblMaxUsers;
+        private System.Windows.Forms.TextBox txtMaxUsers;
+        private System.Windows.Forms.Label lblMaxRooms;
+        private System.Windows.Forms.TextBox txtMaxRooms;
     }
 }
